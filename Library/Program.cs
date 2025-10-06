6e1d14dfcbe6af4748bd8edd9454f9abd96e5e8a
Oisin Quinn <Quinn-O22@ulster.ac.uk>
2025-10-06 15:21:29 +0100

    Updated Code with public access modifiers

--- Library/Program.cs
+++ Library/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Library;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Create a new instance (object) of the Book class
+        // Note how the object name differs from the class name
+        Book book = new Book("C# for beginners", "Bill Gates", "1234567");
+
+        book.DisplayInfo();
+    }
+}
